3a81d6be78583ae37f2b1842311a202b57df5780
Peter Alfonsi <petealft@amazon.com>
2024-01-22 15:27:56 -0800

    fdsd

--- osbenchmark/results_publisher.py
+++ osbenchmark/results_publisher.py
@@ -189,7 +189,8 @@ class SummaryResultsPublisher:
             self._line("Min Throughput", task, throughput["min"], unit, lambda v: "%.2f" % v),
             self._line("Mean Throughput", task, throughput["mean"], unit, lambda v: "%.2f" % v),
             self._line("Median Throughput", task, throughput["median"], unit, lambda v: "%.2f" % v),
-            self._line("Max Throughput", task, throughput["max"], unit, lambda v: "%.2f" % v)
+            self._line("Max Throughput", task, throughput["max"], unit, lambda v: "%.2f" % v),
+            self._publish_percentiles("throughput", task, throughput)
         )
 
     def _publish_latency(self, values, task):
