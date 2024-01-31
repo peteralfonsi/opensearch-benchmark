c2f0389a807f151bc08e6f89882e13fb4ce346d7
Peter Alfonsi <petealft@amazon.com>
2024-01-31 12:25:06 -0800

    more debug

--- osbenchmark/workload/loader.py
+++ osbenchmark/workload/loader.py
@@ -29,6 +29,7 @@ import random
 import re
 import sys
 import tempfile
+import traceback
 import urllib.error
 
 import jinja2
@@ -1282,6 +1283,8 @@ class WorkloadPluginReader:
     def register_standard_value_source(self, op_name, field_name, standard_value_source):
         # Define a value source for parameters for a given operation name and field name, for use in randomization
         try:
+            print("Registering for op {}, field {}. Traceback:".format(op_name, field_name))
+            traceback.print_stack()
             params.register_standard_value_source(op_name, field_name, standard_value_source) # TODO: Should this live in params?
         except exceptions.SystemSetupError:
             print("Attempted to re-register for op {}, field {}, with lambda {}!!".format(op_name, field_name, standard_value_source))
