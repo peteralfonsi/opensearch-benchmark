680507d36597fcd3d9f0d91d3b1c271ea2cbd6ce
Peter Alfonsi <petealft@amazon.com>
2024-01-30 16:04:29 -0800

    silenced repeated registering error for now
    Signed-off-by: Peter Alfonsi <petealft@amazon.com>

--- osbenchmark/workload/loader.py
+++ osbenchmark/workload/loader.py
@@ -1285,7 +1285,9 @@ class WorkloadPluginReader:
         try:
             params.register_standard_value_source(op_name, field_name, standard_value_source) # TODO: Should this live in params?
         except exceptions.SystemSetupError:
-            print("Attempted to re-register for op {}, field {}!!".format(op_name, field_name))
+            #print("Attempted to re-register for op {}, field {}!!".format(op_name, field_name))
+            # TODO: Figure out why this function runs hundreds of times!! 
+            pass
 
     @property
     def meta_data(self):
